1e6d3723273ab3338f70b43bfe46cd2f0f3eb814
Nemiroff <nemiroff@xaker.ru>
2018-03-26 03:59:16 +0500

    v1.0.13 Добавлен BlueBird и специальный тип релиза Remux/Blu-Ray Disc (Отключен по умолчанию)

--- burst/filtering.py
+++ burst/filtering.py
@@ -51,11 +51,12 @@ class Filtering:
     def __init__(self):
         resolutions = OrderedDict()
         resolutions['filter_240p'] = ['240p', u'240р', '_tvrip_', 'satrip', 'vhsrip']
-        resolutions['filter_480p'] = ['480p', u'480р', 'xvid', 'dvd', 'dvdrip', 'hdtv', 'SD']
+        resolutions['filter_480p'] = ['480p', u'480р', 'xvid', 'dvd', 'dvdrip', 'hdtv']
         resolutions['filter_720p'] = ['720p', u'720р', '720i', 'hdrip', 'bluray', 'blu_ray', 'brrip', 'bdrip']
         resolutions['filter_1080p'] = ['1080p', u'1080р', '1080i', 'fullhd', '_fhd_']
         resolutions['filter_1440p'] = ['_2k_', '1440p', u'1440р', '2k', u'2к']
         resolutions['filter_2160p'] = ['_4k_', '2160p', u'2160р', 'uhd', '4k', u'4к']
+        resolutions['filter_remux'] = ['_Remux', '_BD-Remux_', '_Blu-Ray_Disc_']
         self.resolutions = resolutions
 
         self.release_types = {
