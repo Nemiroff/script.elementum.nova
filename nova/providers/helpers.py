630b5acf8d947303e01e3aebcea32e6303c1febd
Nemiroff <nemiroff@xaker.ru>
2018-07-09 00:52:07 +0500

    Возвращение старой нормализации на LostFilm

--- nova/providers/helpers.py
+++ nova/providers/helpers.py
@@ -4,5 +4,5 @@ Helpers for providers that need special filtering
 """
 
 def fix_lf(url):
-    url = url.replace('marvel\'s_', '').replace('dc\'s_', '').replace('s_h_i_e_l_d', 'shield').replace('\'s_', 's_').replace('cloak_dagger', 'cloak_and_dagger')
+    url = url.replace('marvel_s_', '').replace('dc_s_', '').replace('s_h_i_e_l_d', 'shield').replace('_s_', 's_').replace('cloak_dagger', 'cloak_and_dagger')
     return url
