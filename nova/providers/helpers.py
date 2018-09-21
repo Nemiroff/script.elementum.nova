892c05f5bdf0b323b1fe82592790ff6e053b816a
Ruslan <Nemiroff@users.noreply.github.com>
2018-09-21 10:09:26 +0500

    Правки поиска на LostFilm
    Сериал Майянцы

--- nova/providers/helpers.py
+++ nova/providers/helpers.py
@@ -4,5 +4,5 @@ Helpers for providers that need special filtering
 """
 
 def fix_lf(url):
-    url = url.replace('marvel_s_', '').replace('dc_s_', '').replace('s_h_i_e_l_d', 'shield').replace('_s_', 's_').replace('cloak_dagger', 'cloak_and_dagger')
+    url = url.replace('marvel_s_', '').replace('dc_s_', '').replace('s_h_i_e_l_d', 'shield').replace('_s_', 's_').replace('cloak_dagger', 'cloak_and_dagger').replace('_c', 'c')
     return url
